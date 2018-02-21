90afa1ed8d24a2a7a8f2ae76401d2663b84f838c
Tony Colston <tonetheman@gmail.com>
2018-02-21 10:47:31 -0600

    rearranged code with promise still not working

--- bm.js
+++ bm.js
@@ -1,30 +1,31 @@
 
-(
 
-    // maybe load an iframe?
+function onError() {
+    console.log("err!");
+}
+function onGot(current) {
+    console.log("current from onGot",current);
 
-    // old way
-    function() {
-        let c = browser.tabs.getCurrent();
-        let title = browser.tabs.getCurrent().title;
-        let u = browser.tabs.getCurrent().url;
-
-        f = 'https://savedbits.com/save.php?url=' + 
-        //encodeURIComponent(window.location.href) +
-        encodeURIComponent(u) + 
-        //'&title=' + encodeURIComponent(document.title) + '&v=1&';
-        '&title=' + encodeURIComponent(browser.tabs.getCurrent().title) + '&v=1&';    
-        a = function() {       
-            if (!window.open(f + 'noui=1&jump=doclose', 
-                'savedbits', 'location=yes,links=no,scrollbars=no,toolbar=no,width=500,height=300')) 
-                location.href = f + 'jump=yes'    
-        };    
-        if (/Firefox/.test(navigator.userAgent)) {       
-            setTimeout(a, 0)    
-        } else {       
-            a()    
-        }
-
-        console.log(c);
+    let title = current.title;
+    let u = current.url;
+    f = 'https://savedbits.com/save.php?url=' + 
+    encodeURIComponent(u) + 
+    '&title=' + encodeURIComponent(title) + '&v=1&';    
+    a = function() {       
+        if (!window.open(f + 'noui=1&jump=doclose', 
+            'savedbits', 'location=yes,links=no,scrollbars=no,toolbar=no,width=500,height=300')) 
+            location.href = f + 'jump=yes'    
+    };    
+    if (/Firefox/.test(navigator.userAgent)) {       
+        setTimeout(a, 0)    
+    } else {       
+        a()    
     }
-)()
+
+}
+
+// get the current tab
+let gettingCurrent = browser.tabs.getCurrent();
+gettingCurrent.then(onGot, onError);
+
+
